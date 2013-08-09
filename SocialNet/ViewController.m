c182f10ba6a2af2b3db4e63eec03a9289de4b3f4
XaviVaa <x.lan.x@live.com>
2013-08-09 11:08:02 -0500

    Se finalizo el proyecto simple integración con twitter

--- SocialNet/ViewController.m
+++ SocialNet/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <Social/Social.h>
 
 @interface ViewController ()
 
@@ -26,4 +27,30 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)postearRedSocial:(id)sender {
+    SLComposeViewController *redesView = [SLComposeViewController composeViewControllerForServiceType:SLServiceTypeTwitter];
+    if([SLComposeViewController isAvailableForServiceType:SLServiceTypeTwitter])
+    {
+        SLComposeViewControllerCompletionHandler __block completionHandler=^(SLComposeViewControllerResult result)
+        {
+            [redesView dismissViewControllerAnimated:YES completion:nil];
+            
+            switch (result) {
+                case SLComposeViewControllerResultCancelled:
+                    NSLog(@"CANCELADO");
+                    break;
+                case SLComposeViewControllerResultDone:
+                    NSLog(@"ENVIADO");
+                    break;
+                default:
+                    break;
+            }
+        };
+        [redesView setInitialText:@"Probando la integracion con Twitter en iOS"];
+        [redesView addURL:[NSURL URLWithString:@"www.miyosinc.com"]];
+        [redesView addImage:[UIImage imageNamed:@"Miyo.png"]];
+        [redesView setCompletionHandler:completionHandler];
+        [self presentViewController:redesView animated:YES completion:nil];
+    }
+}
 @end
